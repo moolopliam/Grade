8f010ecdfa4e1f7ab30d547b7a8e0bab7c6452d6
moolopliam <44667875+moolopliam@users.noreply.github.com>
2020-03-25 02:05:08 +0700

    commit

---
+++ saladpuk/Grades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace saladpuk
+{
+    internal class Grades
+    {
+        private string _grade;
+
+        public string GradeSubject
+        {
+            get => _grade;
+            set
+            {
+                _grade = value;
+                _grade = Calculate(Convert.ToDouble(_grade));
+            }
+        }
+
+        private string Calculate(double score)
+        {
+            return score > 90 ? "A" :
+                score > 80 ? "B" :
+                score > 70 ? "C" :
+                score > 60 ? "D" : "F";
+        }
+    }
+}
---
+++ saladpuk/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace saladpuk
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var grades = new Grades();
+            Console.Write("Enter score : ");
+            if (double.TryParse(Console.ReadLine(), out var value) && value <= 100)
+            {
+                grades.GradeSubject = value.ToString();
+                Console.WriteLine("results  : {0}", grades.GradeSubject);
+            }
+            else
+            {
+                Console.WriteLine("Please check value");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
